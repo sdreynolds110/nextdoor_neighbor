384017dd362b366134f9db753c4b3e92c71b7e22
Gabby Jones <jones.gm90@gmail.com>
2020-02-20 11:02:21 -0500

    created tables in nextdoorNeighbor db

---
+++ models/restaurants.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+    var restaurants = sequelize.define("Restaurants", {
+        name: {
+            type: DataTypes.STRING,
+            // AllowNull is a flag that restricts a todo from being entered if it doesn't
+            // have a text value
+            allowNull: false,
+            // len is a validation that checks that our todo is between 1 and 140 characters
+            validate: {
+              len: [1, 140]
+            }
+          },
+          zipcode: {
+            type: DataTypes.INTEGER,
+            // defaultValue is a flag that defaults a new todos complete value to false if
+            // it isn't supplied one
+            
+          },
+          address:{
+              type: DataTypes.STRING, 
+              allowNull: false, 
+              validate: {
+                  len: [1,140]
+              }
+          }
+    });
+    return restaurants;
+  };
