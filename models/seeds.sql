0bcf3d6c4971092f5730e1fd9cd131db836bde7b
Gabby Jones <jones.gm90@gmail.com>
2020-02-20 20:35:11 -0500

    updated seed file and pushed data to sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO beauties (name, address, zipcode) VALUES ("Salon 33", "4901 Libbie Mill E Blvd #120 Richmond, VA", 23230);
-INSERT INTO beauties (name, address, zipcode) VALUES ("Rustique Salon and Spa", "444 N Ridge Rd, Henrico, VA ", 23229 );
-INSERT INTO beauties (name, address, zipcode) VALUES ("Flirt", "3407 W Cary St, Richmond, VA", 23221);
+INSERT INTO beauties (name, address, zipcode, createdAt, updatedAt) VALUES ("Salon 33", "4901 Libbie Mill E Blvd #120 Richmond, VA", 23230, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO beauties (name, address, zipcode, createdAt, updatedAt) VALUES ("Rustique Salon and Spa", "444 N Ridge Rd, Henrico, VA ", 23229, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO beauties (name, address, zipcode, createdAt, updatedAt) VALUES ("Flirt", "3407 W Cary St, Richmond, VA", 23221, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
-INSERT INTO restaurants (name, address, zipcode) VALUES ("McLean's Restaurant", "3205 W Broad St, Richmond, VA ", 23230);
-INSERT INTO restaurants (name, address, zipcode) VALUES ("Crafted", "4900 Libbie Mill E Blvd, Richmond, VA", 23230);
-INSERT INTO restaurants (name, address, zipcode) VALUES ("HogsHead Cafe", "9503 W Broad St, Richmond, VA ", 23294);
+INSERT INTO restaurants (name, address, zipcode, createdAt, updatedAt) VALUES ("McLean's Restaurant", "3205 W Broad St, Richmond, VA ", 23230, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO restaurants (name, address, zipcode, createdAt, updatedAt) VALUES ("Crafted", "4900 Libbie Mill E Blvd, Richmond, VA", 23230, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO restaurants (name, address, zipcode, createdAt, updatedAt) VALUES ("HogsHead Cafe", "9503 W Broad St, Richmond, VA ", 23294, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO grocerystores (name, address, zipcode) VALUES ("Ellwood Thompsone", "4 N Thompson St, Richmond, VA", 23221);
-INSERT INTO grocerystores (name, address, zipcode) VALUES ("Little House Green Grocery", "1227 Bellevue Ave, Richmond, VA ", 23227);
-INSERT INTO grocerystores (name, address, zipcode) VALUES ("Libbie Market", "400 Libbie Ave, Richmond, VA", 23226);
+INSERT INTO grocerystores (name, address, zipcode, createdAt, updatedAt) VALUES ("Ellwood Thompsone", "4 N Thompson St, Richmond, VA", 23221, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO grocerystores (name, address, zipcode, createdAt, updatedAt) VALUES ("Little House Green Grocery", "1227 Bellevue Ave, Richmond, VA ", 23227, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO grocerystores (name, address, zipcode, createdAt, updatedAt) VALUES ("Libbie Market", "400 Libbie Ave, Richmond, VA", 23226, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO retails (name, address, zipcode) VALUES ("World of Mirth", "3005 W Cary St, Richmond, VA", 23221);
-INSERT INTO retails (name, address, zipcode) VALUES ("Mod and Soul", "323 W Broad St, Richmond, VA ", 23220);
-INSERT INTO retails (name, address, zipcode) VALUES ("Artemis Vintage", "407 Cleveland St, Richmond, VA", 23221);
+INSERT INTO retails (name, address, zipcode,createdAt, updatedAt) VALUES ("World of Mirth", "3005 W Cary St, Richmond, VA", 23221, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO retails (name, address, zipcode,createdAt, updatedAt) VALUES ("Mod and Soul", "323 W Broad St, Richmond, VA ", 23220, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO retails (name, address, zipcode,createdAt, updatedAt) VALUES ("Artemis Vintage", "407 Cleveland St, Richmond, VA", 23221, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 
