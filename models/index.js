64fe0959452f98812d305a84205724f1ff422079
Gabby Jones <jones.gm90@gmail.com>
2020-02-22 11:11:49 -0500

    created API call

--- models/index.js
+++ models/index.js
@@ -7,11 +7,12 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+require('dotenv').config()
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, process.env.DB_PASS, config);
 }
 
 fs
