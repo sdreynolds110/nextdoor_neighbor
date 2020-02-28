507e224f8ead61d9c5d0db48217461452a67c916
Gabby Jones <jones.gm90@gmail.com>
2020-02-27 20:26:02 -0500

    fixing decimal places

--- models/grocerystores.js
+++ models/grocerystores.js
@@ -1,50 +1,49 @@
 module.exports = function(sequelize, DataTypes) {
-    var GroceryStores = sequelize.define("GroceryStores", {
-      name: {
-        type: DataTypes.STRING,
-        // AllowNull is a flag that restricts a todo from being entered if it doesn't
-        // have a text value
-        allowNull: false,
-        // len is a validation that checks that our todo is between 1 and 140 characters
-        validate: {
-          len: [1, 140]
-        }
-      },
-      zipcode: {
-        type: DataTypes.INTEGER
-        // defaultValue is a flag that defaults a new todos complete value to false if
-        // it isn't supplied one
-      },
-      address:{
-          type: DataTypes.STRING, 
-          allowNull: false, 
-          validate: {
-              len: [1,140]
-          }
-      }, 
-      city:{
-          type: DataTypes.STRING, 
-          allowNull: false,
-      }, 
-      state:{
-          type: DataTypes.STRING, 
-          allowNull: false,
-      }, 
-      lat:{
-        type: DataTypes.DECIMAL, 
-        allowNull: false, 
-        validate: {
-          len: [1, 140]
-        }
-      }, 
-      lng:{
-        type: DataTypes.DECIMAL, 
-        allowNull: false, 
-        validate: {
-          len: [1, 140]
-        }
+  var GroceryStores = sequelize.define("GroceryStores", {
+    name: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
       }
-      
-    });
-    return GroceryStores;
-  };
+    },
+    zipcode: {
+      type: DataTypes.INTEGER
+      // defaultValue is a flag that defaults a new todos complete value to false if
+      // it isn't supplied one
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.DECIMAL(10, 6),
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    lng: {
+      type: DataTypes.DECIMAL(10, 6),
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    }
+  });
+  return GroceryStores;
+};
